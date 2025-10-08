395135a14f4e33e2fb07a5d6708883efc248277c
GabrielBeckler <gabrielbhte0@gmail.com>
2025-10-08 12:26:00 -0300

    Adicionando tudo

---
+++ src/app/dashboard/data/data.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const data = () => {
+  return <h1>data</h1>;
+}   
+
+export default data; 
---
+++ src/app/dashboard/settings/settings.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const settings = () => {
+  return <h1>settings</h1>;
+}   
+
+export default settings; 
